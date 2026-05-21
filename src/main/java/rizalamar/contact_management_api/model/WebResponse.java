faa31e2abd9ab4d74758cffecaea1352aa01f6e7
Rizal amarulloh <rizalamarulloh2014@gmail.com>
2026-05-21 20:15:46 +0700

    feat: add dto

--- src/main/java/rizalamar/contact_management_api/model/WebResponse.java
+++ src/main/java/rizalamar/contact_management_api/model/WebResponse.java
@@ -1,4 +1,15 @@
 package rizalamar.contact_management_api.model;
 
-public class WebResponse {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class WebResponse<T> {
+    private T data;
+    private String errors;
 }
