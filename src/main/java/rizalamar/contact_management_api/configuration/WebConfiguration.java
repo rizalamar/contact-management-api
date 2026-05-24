d9e8a53b99922e5114b88822eadc81fdd5a195c6
Rizal amarulloh <rizalamarulloh2014@gmail.com>
2026-05-24 20:06:30 +0700

    feat: add spring argument resolver

---
+++ src/main/java/rizalamar/contact_management_api/configuration/WebConfiguration.java
@@ -0,0 +1,4 @@
+package rizalamar.contact_management_api.configuration;
+
+public class WebConfiguration {
+}
---
+++ src/main/java/rizalamar/contact_management_api/resolver/UserArgumentResolver.java
@@ -0,0 +1,4 @@
+package rizalamar.contact_management_api.resolver;
+
+public class UserArgumentResolver {
+}
