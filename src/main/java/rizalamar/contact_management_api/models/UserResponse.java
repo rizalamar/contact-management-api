14a0567b083255e284fee5c7a58b3211e297f18c
Rizal amarulloh <rizalamarulloh2014@gmail.com>
2026-05-24 17:06:22 +0700

    feat: add user response

--- src/main/java/rizalamar/contact_management_api/models/UserResponse.java
+++ src/main/java/rizalamar/contact_management_api/models/UserResponse.java
@@ -1,4 +1,16 @@
 package rizalamar.contact_management_api.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class UserResponse {
+    private String username;
+
+    private String name;
 }
