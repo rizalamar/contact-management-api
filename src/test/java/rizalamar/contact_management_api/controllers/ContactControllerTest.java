ae3c81f70a380c55ecbc0d1bb05e0dd86b2f0d28
Rizal amarulloh <rizalamarulloh2014@gmail.com>
2026-05-26 19:33:50 +0700

    feat: feat: update contact test (success, access forbidden for other user tynna to access another contact, update name only test, update bad request)

--- src/test/java/rizalamar/contact_management_api/controllers/ContactControllerTest.java
+++ src/test/java/rizalamar/contact_management_api/controllers/ContactControllerTest.java
@@ -12,6 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 import rizalamar.contact_management_api.entities.Contact;
 import rizalamar.contact_management_api.entities.User;
 import rizalamar.contact_management_api.models.contact.CreateContactRequest;
+import rizalamar.contact_management_api.models.contact.UpdateContactRequest;
 import rizalamar.contact_management_api.repositories.ContactRepository;
 import rizalamar.contact_management_api.repositories.UserRepository;
 
@@ -132,5 +133,159 @@ class ContactControllerTest {
         )
                 .andExpectAll(status().isNotFound())
                 .andExpectAll(jsonPath("$.errors").exists());
+    }
+
+    @Test
+    void getContactForbidden() throws Exception {
+        User otherUser = new User();
+        otherUser.setUsername("user_b");
+        otherUser.setName("User B");
+        otherUser.setPassword("password");
+        otherUser.setToken("test-token-b");
+        otherUser.setTokenExpiredAt(System.currentTimeMillis() + 1000000000L);
+        userRepository.save(otherUser);
+
+        Contact contactB = new Contact();
+        contactB.setId(UUID.randomUUID().toString());
+        contactB.setFirstName("User B");
+        contactB.setLastName("Kontak");
+        contactB.setEmail("userb@mail.com");
+        contactB.setPhone("081234321324");
+        contactB.setUser(otherUser);
+        contactRepository.save(contactB);
+
+        mockMvc.perform(
+                        get("/api/contacts/" + contactB.getId())
+                                .accept(MediaType.APPLICATION_JSON)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .header("X-API-TOKEN", "test-token")
+                )
+                .andExpectAll(status().isNotFound());
+    }
+
+    @Test
+    void updateContactSuccess() throws Exception {
+        User user = userRepository.findById("test").orElseThrow();
+        Contact contact = new Contact();
+        contact.setId(UUID.randomUUID().toString());
+        contact.setFirstName("Rizal");
+        contact.setLastName("Amarulloh");
+        contact.setEmail("rizal@mail.com");
+        contact.setPhone("08123456789");
+        contact.setUser(user);
+        contactRepository.save(contact);
+
+        CreateContactRequest request = new CreateContactRequest();
+        request.setFirstName("Mas Rizal");
+        request.setLastName("Amarulloh");
+        request.setEmail("rizal@mail.com");
+        request.setPhone("08123456789");
+
+        String requestJson = objectMapper.writeValueAsString(request);
+
+        mockMvc.perform(
+                put("/api/contacts/" + contact.getId())
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("X-API-TOKEN", "test-token")
+                        .content(requestJson)
+        )
+                .andExpectAll(status().isOk())
+                .andExpectAll(jsonPath("$.data.id").value(contact.getId()))
+                .andExpectAll(jsonPath("$.data.firstName").value(request.getFirstName()))
+                .andExpectAll(jsonPath("$.data.lastName").value(request.getLastName()))
+                .andExpectAll(jsonPath("$.data.email").value(request.getEmail()))
+                .andExpectAll(jsonPath("$.data.phone").value(request.getPhone()));
+    }
+
+    @Test
+    void updateContactFirstNameOnlySuccess() throws Exception {
+        User user = userRepository.findById("test").orElseThrow();
+        Contact contact = new Contact();
+        contact.setId(UUID.randomUUID().toString());
+        contact.setFirstName("Rizal");
+        contact.setLastName("Amarulloh");
+        contact.setEmail("rizal@mail.com");
+        contact.setPhone("08123456789");
+        contact.setUser(user);
+        contactRepository.save(contact);
+
+        UpdateContactRequest request = new UpdateContactRequest();
+        request.setFirstName("Mas Rizal");
+
+        String requestJson = objectMapper.writeValueAsString(request);
+
+        mockMvc.perform(
+                put("/api/contacts/" + contact.getId())
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("X-API-TOKEN", "test-token")
+                        .content(requestJson)
+        )
+                .andExpectAll(status().isOk())
+                .andExpectAll(jsonPath("$.data.id").value(contact.getId()))
+                .andExpectAll(jsonPath("$.data.firstName").value("Mas Rizal"))
+                .andExpectAll(jsonPath("$.data.lastName").isNotEmpty())
+                .andExpectAll(jsonPath("$.data.email").isNotEmpty())
+                .andExpectAll(jsonPath("$.data.phone").isNotEmpty());
+    }
+
+    @Test
+    void updateContactBadRequest() throws Exception{
+        User user = userRepository.findById("test").orElseThrow();
+        Contact contact = new Contact();
+        contact.setId(UUID.randomUUID().toString());
+        contact.setFirstName("Rizal");
+        contact.setLastName("Amarulloh");
+        contact.setEmail("rizal@mail.com");
+        contact.setPhone("08123456789");
+        contact.setUser(user);
+        contactRepository.save(contact);
+
+        UpdateContactRequest request = new UpdateContactRequest();
+        request.setFirstName("");
+        request.setLastName("");
+        request.setEmail("salah-email");
+        request.setPhone("");
+
+        String requestJson = objectMapper.writeValueAsString(request);
+
+        mockMvc.perform(
+                put("/api/contacts/" + contact.getId())
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("X-API-TOKEN", "test-token")
+                        .content(requestJson)
+        )
+                .andExpectAll(status().isBadRequest())
+                .andExpectAll(jsonPath("$.errors").exists());
+    }
+
+    @Test
+    void updateContactInvalidToken() throws Exception {
+        User user = userRepository.findById("test").orElseThrow();
+        Contact contact = new Contact();
+        contact.setId(UUID.randomUUID().toString());
+        contact.setFirstName("Rizal");
+        contact.setLastName("Amarulloh");
+        contact.setEmail("rizal@mail.com");
+        contact.setPhone("08123456789");
+        contact.setUser(user);
+        contactRepository.save(contact);
+
+        UpdateContactRequest request = new UpdateContactRequest();
+        request.setFirstName("Mas Rizal");
+
+        String requestJson = objectMapper.writeValueAsString(request);
+
+        mockMvc.perform(
+                put("/api/contacts/" + contact.getId())
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("X-API-TOKEN", "test-salah")
+                        .content(requestJson)
+        )
+                .andExpectAll(status().isUnauthorized())
+                .andExpectAll(jsonPath("$.errors").isNotEmpty());
     }
 }
