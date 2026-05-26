a953d587e64433107087eaac723461f1e415fe6e
Rizal amarulloh <rizalamarulloh2014@gmail.com>
2026-05-26 09:36:13 +0700

    update: contact repository by adding find contact by user and id contact

--- src/main/java/rizalamar/contact_management_api/repositories/ContactRepository.java
+++ src/main/java/rizalamar/contact_management_api/repositories/ContactRepository.java
@@ -3,7 +3,11 @@ package rizalamar.contact_management_api.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import rizalamar.contact_management_api.entities.Contact;
+import rizalamar.contact_management_api.entities.User;
+
+import java.util.Optional;
 
 @Repository
 public interface ContactRepository extends JpaRepository<Contact, String> {
+    Optional<Contact> findFirstByUserAndId(User user, String id);
 }
