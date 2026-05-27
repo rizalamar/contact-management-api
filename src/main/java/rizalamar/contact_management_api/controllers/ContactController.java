a682ca1d3ff7425ddc01069cf9c39b0f82f69bae
Rizal amarulloh <rizalamarulloh2014@gmail.com>
2026-05-27 19:33:25 +0700

    feat: pagination

--- src/main/java/rizalamar/contact_management_api/controllers/ContactController.java
+++ src/main/java/rizalamar/contact_management_api/controllers/ContactController.java
@@ -1,15 +1,20 @@
 package rizalamar.contact_management_api.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import rizalamar.contact_management_api.entities.Contact;
 import rizalamar.contact_management_api.entities.User;
+import rizalamar.contact_management_api.models.PagingResponse;
 import rizalamar.contact_management_api.models.WebResponse;
 import rizalamar.contact_management_api.models.contact.ContactResponse;
 import rizalamar.contact_management_api.models.contact.CreateContactRequest;
+import rizalamar.contact_management_api.models.contact.SearchContactRequest;
 import rizalamar.contact_management_api.models.contact.UpdateContactRequest;
 import rizalamar.contact_management_api.services.ContactService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/contacts")
@@ -41,4 +46,32 @@ public class ContactController {
         contactService.delete(user, contactId);
         return WebResponse.<String>builder().data("OK").build();
     }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public WebResponse<List<ContactResponse>> search(User user,
+                                                     @RequestParam(value = "name", required = false) String name,
+                                                     @RequestParam(value = "email", required = false) String email,
+                                                     @RequestParam(value = "phone", required = false) String phone,
+                                                     @RequestParam(value = "page", required = false, defaultValue = "0") Integer page,
+                                                     @RequestParam(value = "size", required = false, defaultValue = "10") Integer size
+    ) {
+        SearchContactRequest request = SearchContactRequest.builder()
+                .name(name)
+                .email(email)
+                .phone(phone)
+                .page(page)
+                .size(size)
+                .build();
+
+        Page<ContactResponse> contactResponses = contactService.search(user, request);
+        return WebResponse.<List<ContactResponse>>builder()
+                .data(contactResponses.getContent())
+                .paging(PagingResponse.builder()
+                        .currentPage(contactResponses.getNumber())
+                        .totalPages(contactResponses.getTotalPages())
+                        .size(contactResponses.getSize())
+                        .build())
+                .build();
+    }
+
 }
--- src/main/java/rizalamar/contact_management_api/services/ContactService.java
+++ src/main/java/rizalamar/contact_management_api/services/ContactService.java
@@ -1,6 +1,12 @@
 package rizalamar.contact_management_api.services;
 
+import jakarta.persistence.criteria.Predicate;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,11 +15,15 @@ import rizalamar.contact_management_api.entities.Contact;
 import rizalamar.contact_management_api.entities.User;
 import rizalamar.contact_management_api.models.contact.ContactResponse;
 import rizalamar.contact_management_api.models.contact.CreateContactRequest;
+import rizalamar.contact_management_api.models.contact.SearchContactRequest;
 import rizalamar.contact_management_api.models.contact.UpdateContactRequest;
 import rizalamar.contact_management_api.repositories.ContactRepository;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -75,6 +85,45 @@ public class ContactService {
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Contact not found"));
 
         contactRepository.delete(contact);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ContactResponse> search (User user, SearchContactRequest request){
+        Specification<Contact> specification = (root, query, builder) -> {
+            List<Predicate> predicates = new ArrayList<>();
+
+            // kondisi 1: harus milik user yang sedang login
+            predicates.add(builder.equal(root.get("user"), user));
+
+            // kondisi 2: filter nama(firstname atau lastname)
+            if(Objects.nonNull(request.getName())) {
+                predicates.add(builder.or(
+                        builder.like(root.get("firstName"), "%" + request.getName() + "%"),
+                        builder.like(root.get("lastName"), "%" + request.getName() + "%")
+                ));
+            }
+
+            // kondisi 3: filter email
+            if(Objects.nonNull(request.getEmail())) {
+                predicates.add(builder.like(root.get("email"), "%" + request.getEmail() + "%"));
+            }
+
+            // kondisi 4: filter phone
+            if(Objects.nonNull(request.getPhone())) {
+                predicates.add(builder.like(root.get("phone"), "%" + request.getPhone() + "%"));
+            }
+
+            return query.where(predicates.toArray(new Predicate[]{})).getRestriction();
+        };
+
+        Pageable pageable = PageRequest.of(request.getPage(), request.getSize());
+        Page<Contact> contacts = contactRepository.findAll(specification, pageable);
+
+        List<ContactResponse> contactResponses = contacts.getContent().stream()
+                .map(this::toContactResponse)
+                .collect(Collectors.toList());
+
+        return new PageImpl<>(contactResponses, pageable, contacts.getTotalElements());
     }
 
 
